68d2f137079d91f9f9fbc0a1cb4b72d0524c9c02
PatrickTheCodeGuy <patrick.jthompson93@gmail.com>
2018-10-05 08:53:50 -0700

    Done with mvp

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const cors = require("cors");
 server.use(cors());
 server.use(express.json());
 
+//Project Routes
 server.get("/api/projects/", (req, res) => {
 	dbProjects
 		.get()
@@ -27,6 +28,21 @@ server.get("/api/projects/:id", (req, res) => {
 		.get(id)
 		.then(post => {
 			res.status(200).json(post);
+		})
+		.catch(() => {
+			res.status(500).json({ error: "Could not load posts." });
+		});
+});
+
+server.get("/api/projects/getactions/:id", (req, res) => {
+	const { id } = req.params;
+	if (!id) {
+		res.status(500).json({ error: "No ID." });
+	}
+	dbProjects
+		.getProjectActions(id)
+		.then(actions => {
+			res.status(200).json(actions);
 		})
 		.catch(() => {
 			res.status(500).json({ error: "Could not load posts." });
@@ -86,6 +102,99 @@ server.delete("/api/projects/:id", (req, res) => {
 					res.status(500).json({ error: "This post could not be deleted." });
 				} else {
 					res.status(200).json({ Success: "Post has been deleted!" });
+				}
+			})
+			.catch(() => {
+				res.status(500).json({ error: "id does not exist." });
+			});
+	}
+});
+
+//Actions ROutes
+
+server.get("/api/projects/actions/", (req, res) => {
+	dbActions
+		.get()
+		.then(action => {
+			res.status(200).json(action);
+		})
+		.catch(() => {
+			res.status(500).json({ error: "Could not load actions!." });
+		});
+});
+
+server.get("/api/projects/actions/:id", (req, res) => {
+	const { id } = req.params;
+	if (!id) {
+		res.status(500).json({ error: "No ID." });
+	}
+	dbActions
+		.get(id)
+		.then(post => {
+			res.status(200).json(post);
+		})
+		.catch(() => {
+			res.status(500).json({ error: "Could not load actions." });
+		});
+});
+
+server.post("/api/projects/actions/", (req, res) => {
+	const { project_id, description, notes } = req.body;
+	if (!notes || !description || !project_id) {
+		res.status(400).json({
+			error:
+				"You did not provide an action project id, description, or additional notes."
+		});
+	} else {
+		const newProject = { project_id, description, notes };
+		dbActions
+			.insert(newProject)
+			.then(project => {
+				res.status(200).json(project);
+			})
+			.catch(() => {
+				res.status(500).json({
+					error: "There was an error while saving this action to the project."
+				});
+			});
+	}
+});
+
+server.put("/api/projects/actions/:id", (req, res) => {
+	const { id } = req.params;
+	const { project_id, description, notes } = req.body;
+	if (!notes || !description || !project_id) {
+		res.status(400).json({
+			error:
+				"Please provide a project id, a description, and additional notes for this action."
+		});
+	} else if (!id) {
+		res.status(500).json(null);
+	} else {
+		const thisAction = { project_id, description, notes };
+		dbActions
+			.update(id, thisAction)
+			.then(isUpdated => {
+				res.status(200).json(isUpdated);
+			})
+			.catch(() => {
+				res.status(404).json({ error: "Could not update action." });
+			});
+	}
+});
+
+server.delete("/api/projects/actions/:id", (req, res) => {
+	const { id } = req.params;
+	if (!id) {
+		res.status(500).json({ error: "Could not delete, id not found" });
+	} else {
+		dbActions
+			.remove(id)
+			.then(removedPost => {
+				if (removedPost === 0) {
+					res.status(500).json({ error: "This action could not be deleted." });
+				} else {
+					res.status(200).json({ Success: "Action has been deleted!" });
 				}
 			})
 			.catch(() => {
